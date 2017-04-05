122c2b73515969effaffc9a07de642776820647c
Christophe Mangeat <christophe.mangeat@camptocamp.com>
2017-04-05 11:09:10 +0200

    Add an indirection, depending on osname, switching beetwen equals and equalsIgnoreCase in IOUtils.rename.
    IOUtils.rename method does nothing when src and target names just differ depending on the case.
    Such a behaviour is appropriate with windows (non case-sensitive for filename) but harms with linux, cf geos-7946.

--- src/main/src/test/java/org/geoserver/data/util/IOUtilsTest.java
+++ src/main/src/test/java/org/geoserver/data/util/IOUtilsTest.java
@@ -6,9 +6,15 @@
 package org.geoserver.data.util;
 
 import com.google.common.io.Files;
-import org.junit.Rule;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -17,12 +23,31 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.zip.ZipOutputStream;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ System.class, IOUtils.class})
 public class IOUtilsTest {
 
-    @Rule
-    public TemporaryFolder temp = new TemporaryFolder(new File("target"));
+    TemporaryFolder folder;
+    TemporaryFolder temp;
+
+    @Before
+    public void initTmpFolder() throws IOException {
+        folder = new TemporaryFolder();
+        folder.create();
+        temp = new TemporaryFolder(new File("target"));
+        temp.create();
+    }
+
+    @After
+    public void deleteTmpFolder() throws IOException {
+        folder.delete();
+        temp.delete();
+    }
+
 
     @Test
     public void testZipUnzip() throws IOException {
@@ -42,5 +67,61 @@ public class IOUtilsTest {
         IOUtils.decompress(bin, p2.toFile());
 
         assertTrue(p2.resolve("foo/bar/bar.txt").toFile().exists());
+    }
+
+    @Test
+    public void renameDirNamesDifferLinux() throws IOException, InterruptedException {
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.getProperty("os.name")).thenReturn("linux");
+        String newName = "DirB";
+
+        attemptRename("DirA", newName);
+
+        assertEquals(newName, folder.getRoot().list()[0]);
+    }
+
+    @Test
+    public void renameDirNamesCaseDifferLinux() throws IOException, InterruptedException {
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.getProperty("os.name")).thenReturn("linux");
+        String newName = "Dira";
+
+        attemptRename("DirA", newName);
+
+        assertEquals(newName, folder.getRoot().list()[0]);
+    }
+
+
+    @Test
+    public void renameDirNamesDifferWindows() throws IOException, InterruptedException {
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.getProperty("os.name")).thenReturn("Windows");
+        String newName = "DirB";
+
+        attemptRename("DirA", newName);
+
+        assertEquals(newName, folder.getRoot().list()[0]);
+
+    }
+
+    @Test
+    public void renameDirNamesCaseDifferWindows() throws IOException, InterruptedException {
+        PowerMockito.mockStatic(System.class);
+        Mockito.when(System.getProperty("os.name")).thenReturn("Windows");
+        String oldName = "DirA";
+        String newName = "Dira";
+
+        attemptRename("DirA", newName);
+
+        assertEquals(oldName, folder.getRoot().list()[0]);
+    }
+
+    private void attemptRename(String oldName, String newName) throws IOException {
+        File toBeRenamed = folder.newFolder(oldName);
+        assertEquals(1, folder.getRoot().list().length);
+
+        IOUtils.rename(toBeRenamed, newName);
+
+        assertEquals(1, folder.getRoot().list().length);
     }
 }
