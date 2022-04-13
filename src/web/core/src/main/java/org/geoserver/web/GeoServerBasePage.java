5cdecc3a956ffb8014e0f5de7d3ee33047846ec1
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2022-04-13 20:16:32 +0200

    fixing lost customization in GeoServerBasePage class

--- src/web/core/src/main/java/org/geoserver/web/GeoServerBasePage.java
+++ src/web/core/src/main/java/org/geoserver/web/GeoServerBasePage.java
@@ -40,6 +40,7 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.resource.JQueryResourceReference;
+import org.georchestra.GeorchestraHeaderIframe;
 import org.geoserver.catalog.Catalog;
 import org.geoserver.config.GeoServer;
 import org.geoserver.ows.URLMangler;
@@ -91,7 +92,7 @@ public class GeoServerBasePage extends WebPage implements IAjaxIndicatorAware {
 
     @SuppressWarnings("serial")
     public GeoServerBasePage() {
-
+        add(new GeorchestraHeaderIframe("georchestraIframe"));
         // lookup for a pluggable favicon
         PackageResourceReference faviconReference = null;
         List<HeaderContribution> cssContribs =
