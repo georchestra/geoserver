9ff6110c4d971fbfd21a65e98b3ba5d0b08ea28b
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2023-02-21 15:23:37 +0100

    customizations applied on top of geoserver 2.22.2 for geOrchestra

--- src/web/core/src/test/java/org/geoserver/web/GeoServerAboutPageTest.java
+++ src/web/core/src/test/java/org/geoserver/web/GeoServerAboutPageTest.java
@@ -9,11 +9,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 import org.apache.wicket.util.tester.TagTester;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class GeoServerAboutPageTest extends GeoServerWicketTestSupport {
 
     @Test
+    @Ignore("No login form in about page of geOrchestra's geoserver")
     public void testLoginFormAction() throws Exception {
         logout();
         tester.executeUrl("./wicket/bookmarkable/org.geoserver.web.AboutGeoServerPage");
