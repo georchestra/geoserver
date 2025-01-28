75a2d428853fbc2f116912a447c71f81a3b58a17
Landry Breuil <breuil@craig.fr>
2025-01-28 11:00:37 +0100

    georchestra integration - integrate the georchestra webcomponent header
    reach into the datadir to read default.properties for height/url/script/logo/configfile/etc
    cf georchestra/geoserver@fb69309a8
    and georchestra/geoserver@af88b9cc401 for the previous header bits
    includes #38 for the config file
    
    was 1ddcec7d8 and 6f837063e in 2.26.1-georchestra

---
+++ src/web/core/src/main/java/org/georchestra/GeorchestraHeaderWebComponent.java
@@ -0,0 +1,51 @@
+package org.georchestra;
+
+import java.util.logging.Logger;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebComponent;
+import org.geoserver.web.GeoServerApplication;
+import org.geotools.util.logging.Logging;
+
+public class GeorchestraHeaderWebComponent extends WebComponent {
+
+    private String headerUrl;
+    private String headerHeight;
+    private String legacyHeader;
+    private String logoUrl;
+    private String georchestraStylesheet;
+    private String headerConfigFile;
+
+    private static Logger LOGGER = Logging.getLogger(GeorchestraHeaderWebComponent.class);
+
+    private void init() {
+        headerHeight = getGeoServerApplication().getBean("georchestraHeaderHeight").toString();
+        headerUrl = getGeoServerApplication().getBean("georchestraHeaderUrl").toString();
+        legacyHeader = getGeoServerApplication().getBean("georchestraLegacyHeader").toString();
+        logoUrl = getGeoServerApplication().getBean("georchestraLogoUrl").toString();
+        georchestraStylesheet =
+                getGeoServerApplication().getBean("georchestraStylesheet").toString();
+        headerConfigFile =
+                getGeoServerApplication().getBean("georchestraHeaderConfigFile").toString();
+    }
+
+    protected GeoServerApplication getGeoServerApplication() {
+        return (GeoServerApplication) getApplication();
+    }
+
+    public GeorchestraHeaderWebComponent(String id) {
+        super(id);
+        init();
+    }
+
+    @Override
+    protected void onComponentTag(ComponentTag tag) {
+        tag.put("active-app", "geoserver");
+        tag.put("legacy-url", this.headerUrl);
+        tag.put("legacy-header", this.legacyHeader);
+        tag.put("logo-url", this.logoUrl);
+        tag.put("config-file", this.headerConfigFile);
+        tag.put("stylesheet", this.georchestraStylesheet);
+        tag.put("height", this.headerHeight);
+        super.onComponentTag(tag);
+    }
+}
