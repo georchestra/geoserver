b7827a2131260c49da618b9b3f46db4ba539c52f
Landry Breuil <breuil@craig.fr>
2026-06-19 15:21:13 +0200

    fix: fixing versions / imports in the pom/sources

--- src/main/src/main/java/org/geoserver/security/filter/GeoServerSecurityContextPersistenceFilter.java
+++ src/main/src/main/java/org/geoserver/security/filter/GeoServerSecurityContextPersistenceFilter.java
@@ -11,7 +11,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.function.Supplier;
 import org.geoserver.security.GeoServerSecurityFilterChainProxy;
 import org.geoserver.security.config.SecurityContextPersistenceFilterConfig;
 import org.geoserver.security.config.SecurityNamedServiceConfig;
